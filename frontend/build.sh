19f81f5495ce0a233ca40595a77e0cc4a334d11b
Ramana Kondaveeti <ramana.awstech@gmail.com>
2026-05-24 13:04:50 +0530

    docker update

--- frontend/build.sh
+++ frontend/build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 if [ ! -d "build" ]; then
-  echo "⚠️ Build folder not found. Running build..."
-  npm install && npm run build
+  echo "Build folder not found. Running build..."
+  export NODE_OPTIONS="${NODE_OPTIONS:---max-old-space-size=1536}"
+  npm install --prefer-offline --no-audit --progress=false && npm run build
 else
-  echo "✅ Build folder already exists. Skipping build."
+  echo "Build folder already exists. Skipping build."
 fi
